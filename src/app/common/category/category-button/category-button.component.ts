d5184a138ca56c804dd82d161d6e2cc83ede5041
Tomasz Buga <kontakt@tomaszbuga.pl>
2021-10-03 20:27:14 +0200

    fixing code quality

--- src/app/common/category/category-button/category-button.component.ts
+++ src/app/common/category/category-button/category-button.component.ts
@@ -2,8 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  templateUrl: './category-button.component.html',
-  styleUrls: ['./category-button.component.scss']
+  templateUrl: './category-button.component.html'
 })
 export class CategoryButtonComponent implements OnInit {
   @Input() buttonTitle: string;
